80e975d01add561012076c79c37deeb0291dfe9e
typpo <ian@typpo.us>
2010-11-26 21:54:09 +0000

    working on dynamic form

--- notes/forms.py
+++ notes/forms.py
@@ -8,8 +8,13 @@ class InterviewForm(ModelForm):
     company = forms.CharField(max_length=50)
     position = forms.CharField(max_length=50)
     date = forms.DateField()
-    description = forms.CharField(max_length=2000, label='', \
-        widget=TinyMCE(attrs={'rows':20,'cols':80}))
+    description = forms.CharField(max_length=2000, \
+        widget=TinyMCE(attrs={'rows':15,'cols':80}))
+
+    question = forms.CharField(max_length=2000, \
+        widget=TinyMCE(attrs={'rows':10,'cols':80}))
+    answer = forms.CharField(max_length=2000, \
+        widget=TinyMCE(attrs={'rows':10,'cols':80}))
 
     class Meta:
         model = FlatPage
